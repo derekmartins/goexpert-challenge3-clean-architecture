d0acfc747278e449249312e8c8eb527d2cb0606f
derekribeiromartins <derek.ribeirom@gmail.com>
2025-02-21 17:50:21 -0300

    add initial structure of project

---
+++ tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+// +build tools
+
+package goexpert_challenge3_clean_architecture
+
+import (
+	_ "github.com/99designs/gqlgen"
+	_ "github.com/99designs/gqlgen/graphql/introspection"
+)
